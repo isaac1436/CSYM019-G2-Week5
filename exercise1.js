a9fe3d4ee76c577fbed008133d7f0481bc09fc52
ISAAC DESMOND NRUEM <isaac1436@student.babcock.edu.ng>
2024-03-01 16:44:37 +0000

    element with id 'circle' now chnages color on click

--- exercise1.js
+++ exercise1.js
@@ -1,5 +1,10 @@
 function myFunc(){
-    
+    var circle=document.getElementById('circle');
+
+    function change(){
+        circle.style.backgroundColor='blue';
+    }
+    circle.addEventListener('click',change);
 }
 
 document.addEventListener('DOMContentLoaded',myFunc);
